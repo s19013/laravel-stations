3b8a433057ce098a55c9ee57d69628d72125b050
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-04 22:38:29 +0900

    Movieのバリデーションはこのファイルにまとめた

---
+++ app/Http/Requests/MovieRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MovieRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // 一部urlでのみ有効
+        dd($this->path());
+        $validityList = [
+            '/admin/movie/store'
+        ];
+
+        if (in_array($this->path(), $validityList )) { return true; }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title'       => 'required',
+            'image_url'   => 'required|url',
+            'description' => 'required',
+            'is_showing'  => 'required',
+            'published_year' => 'required',
+        ];
+    }
+
+    public function message()
+    {
+        return [
+            'title.required'       => '入力してください',
+            'image_url.required'   => '入力してください',
+            'image_url.url'        => 'urlを入力してください',
+            'description.required' => '入力してください',
+            'is_showing.required'  => '入力してください',
+            'published_year.required' => '入力してください',
+        ];
+    }
+}
