6fa03f2a1366fd5766471dcdd6ef737794e12d88
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-04 22:39:06 +0900

    複数の関数追加

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -5,7 +5,37 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use DB;
+
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    // タイトルにかぶりがないかチェック
+    public static function  isAllreadyExists($title)
+    {
+        return Movie::where('title','=',$title)
+        ->exists();//existsでダブっていればtrue
+    }
+
+    // 登録
+    public static function storeMovie($title,$image_url,$description,$is_showing,$published_year)
+    {
+        if (empty($is_showing)) {$is_showing = false;}
+        DB::transaction(function () use($title,$image_url,$description,$is_showing,$published_year){
+            Movie::create([
+                'title'     => $title,
+                'image_url' => $image_url,
+                'description' => $description,
+                'is_showing'  => $is_showing,
+                'published_year' => $published_year,
+            ]);
+        });
+    }
 }
