e988dde2b7fc7f6e9937ae679d89d504032a1771
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-06 01:45:50 +0900

    色々追加

--- app/Http/Controllers/AdminMovieController.php
+++ app/Http/Controllers/AdminMovieController.php
@@ -20,24 +20,35 @@ class AdminMovieController extends Controller
     public function store(MovieRequest $request)
     {
         // 登録
-        Movie::storeMovie(
-            title         :$request->title,
-            image_url     :$request->image_url,
-            description   :$request->description,
-            is_showing    :$request->is_showing,
-            published_year:$request->published_year
-        );
+        Movie::storeMovie($request);
 
+        // 外部結合してないからこの書き方でも n+1は起きないはず
         $movieList = Movie::all();
-        return redirect('/admin/movies')->with([
+        return redirect('/admin/movies/')->with([
             'movieList' => $movieList,
             'message'   => '映画を追加しました',
+        ]);
+    }
+
+    public function update(MovieRequest $request)
+    {
+        Movie::updateMovie($request);
+
+        $movieList = Movie::all();
+        return redirect('/admin/movies/')->with([
+            'movieList' => $movieList,
+            'message'   => '更新しました',
         ]);
     }
 
     public function transitionToCreate()
     {
         return view('admin.movie.create');
+    }
+
+    public function transitionToEdit($id)
+    {
+        return view('admin.movie.edit')->with(['movie' => Movie::getMovieData($id)]);
     }
 
 
