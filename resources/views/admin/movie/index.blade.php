79dcba9cb62b8f34ac7f8f1f519393ff139de1fa
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-06 22:07:58 +0900

    やはりこちらの方が良い?

--- resources/views/admin/movie/index.blade.php
+++ resources/views/admin/movie/index.blade.php
@@ -28,11 +28,9 @@
             <th></th>
             @foreach ($movieList as $movie)
                 <td>
-                    {{-- <form action="/admin/movies/{{$movie->id}}/destroy" method="post" onsubmit="return finalCheck()"> --}}
-                    <form action="{{route('movie.delete',$movie->id)}}" method="post" onsubmit="return finalCheck()">
+                    <form action="/admin/movies/{{$movie->id}}/destroy" method="post" onsubmit="return finalCheck()">
                         @csrf
                         @method('DELETE')
-                        {{-- <input type="hidden" name="_method" value="DELETE"> --}}
                         <input type="submit" id='delete' value="削除">
                     </form>
                 </td>
