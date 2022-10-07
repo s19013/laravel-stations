fa059be0931eac4c8def403a37bd60f583bcdfdb
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-07 21:58:00 +0900

    検索用の関数を作った

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\tool\searchToolKit;
+
 use DB;
 
 class Movie extends Model
@@ -50,6 +52,34 @@ class Movie extends Model
         DB::transaction(function () use($id){
             Movie::where('id', '=',$id)->delete();
         });
+    }
+
+    public static function search($request)
+    {
+        $searchToolKit = new searchToolKit();
+
+        // %と_をエスケープ
+        $escaped = $searchToolKit->sqlEscape($articleToSearch);
+
+        //and検索のために空白区切りでつくった配列を用意
+        $wordListToSearch = $searchToolKit->preparationToAndSearch($escaped);
+
+
+        // 基本クエリ
+        $query = Movie::select('*');
+
+        // キーワード追加
+        foreach($wordListToSearch as $word){
+            $query->where('title','like',$word);
+            $query->where('description','like',$word);
+        }
+
+        // 検索対象
+        if ($request->target == "showing") { $query->where('is_showing','=',1); }
+        if ($request->target == "not showing") { $query->where('is_showing','=',0); }
+
+        // 取得
+        return $query->get();
     }
 
     // 単体データ取得
