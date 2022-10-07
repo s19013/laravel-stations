046be909da8cf3fb7173b285abcca52f1efaa29a
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-07 21:57:33 +0900

    エスケープしたり配列かするツールをつくた

---
+++ app/tool/searchToolKit.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\tool;
+
+use Illuminate\Http\Request;
+
+class searchToolKit extends Controller
+{
+    //sqlでlike検索する前にするエスケープ処理
+    public function sqlEscape($arg)
+    {
+        // %をエスケープ
+        $escaped = preg_replace(
+        '/%/',
+        '\%',
+        $arg);
+
+        // _をエスケープ
+        $escaped = preg_replace(
+        '/_/',
+        '\_',
+        $escaped);
+
+        return $escaped;
+    }
+
+    //and検索できるように空白で区切って､配列にする
+    public function preparationToAndSearch($arg)
+    {
+        // 全角スペースを半角に変換
+        $spaceConversion = mb_convert_kana($arg, 's');
+
+        // 単語を半角スペースで区切り、配列にする
+        return preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
+
+    }
+}
