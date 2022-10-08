b4bbdb97c0f3838480cd2c5d1d9594c6eb27f9e3
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-08 22:37:31 +0900

    新規作成

---
+++ database/migrations/2022_10_08_222140_create_schedules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('movie_id');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamps();
+
+            // 外部キー
+            $table->foreign('movie_id')->references('id')->on('movies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
+
