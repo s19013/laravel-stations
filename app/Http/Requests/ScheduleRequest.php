728a7d10862808af649ec545d3ae2b2465507ee3
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-09 21:00:36 +0900

    formRequestを作った

---
+++ app/Http/Requests/ScheduleRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+// use Validator;
+
+use Illuminate\Validation\Validator;
+
+class ScheduleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // 通信メソッドで制限
+        // 今回の仕様ではURLで制限をかけるのは難しいと判断
+        $validityList = [
+            'PATCH',
+            'POST'
+        ];
+
+        if (in_array($this->method(), $validityList )) { return true; }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        // そもそもなんでチェックボックスにこだわるんだよトグルとか､ラジオボタンで十分でしょ!!
+        // 2バイトトラップの処理とかあるけど今回はそういうの無視
+        return [
+            'start_time_date' => 'required|date|date_format:Y-m-d',
+            'start_time_time' => 'required|date_format:H:i',
+            'end_time_date'   => 'required|date|date_format:Y-m-d',
+            'end_time_time'   => 'required|date_format:H:i',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'start_time_date.required'    => '入力してください',
+            'start_time_date.date_format' => '年-月-日の形式で入力してください',
+            'start_time_time.required'    => '入力してください',
+            'start_time_time.date_format' => '時間:分の形式で入力してください',
+            'end_time_date.required'      => '入力してください',
+            'end_time_date.date_format'   => '年-月-日の形式で入力してください',
+            'end_time_time.required'      => '入力してください',
+            'end_time_time.date_format'   => '時間:分の形式で入力してください',
+        ];
+    }
+}
