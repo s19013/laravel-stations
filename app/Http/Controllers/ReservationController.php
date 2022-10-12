c024e32cb5773457a1c4baa02b0ffe29e2c0ff1b
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-12 13:33:01 +0900

    生成

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reservation;
+use App\Models\Movie;
+use App\Models\Sheet;
 
 class ReservationController extends Controller
 {
-    //
+    public function index($movie_id,$schedule_id,Request $request)
+    {
+        // クエリがないなら400
+        if (empty($request->screening_date)) {abort(400);}
+
+       return view('sheet.index', [
+           "movie_id"       => $movie_id,
+           "schedule_id"    => $schedule_id,
+           "screening_date" => $request->screening_date,
+           "sheets" => Sheet::all()
+       ]);
+    }
+
+    public function create($movie_id,$schedule_id,Request $request)
+    {
+        // クエリがないなら400
+        if (empty($request->screening_date) || empty($request->sheet_id)) {abort(400);}
+
+        return view('movie.reservation', [
+            "movie_id"       => $movie_id,
+            "schedule_id"    => $schedule_id,
+            "screening_date" => $request->screening_date,
+            "sheet_id"        => $request->sheet_id
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name'     =>'required|max:255',
+            'email'    =>'required|max:255|email',
+            "schedule_id" =>'required',
+            "screening_date" =>'required',
+            "sheet_id" =>'required',
+        ]);
+
+        // すでに予約されてないか
+        if (Reservation::isAllReadyExist($request)) {
+            // return redirect("movies/{$request->movie_id}")->with([
+            //     "message"        => "そこはすでに予約されています",
+            //     "movie_id"       => $movie_id,
+            //     "schedule_id"    => $schedule_id,
+            //     "screening_date" => $request->screening_date,
+            //     "sheets" => Sheet::all()
+            // ]);
+            return redirect("/movies/{$request->movie_id}/schedules/{$request->schedule_id}/sheets?screening_date={$request->screening_date}")->with([
+                "message"        => "そこはすでに予約されています",
+                "movie_id"       => $request->movie_id,
+                "schedule_id"    => $request->schedule_id,
+                "screening_date" => $request->screening_date,
+                "sheets" => Sheet::all()
+            ]);
+        }
+
+        Reservation::storeReservateion($request);
+
+        return redirect("movies/{$request->movie_id}")->with([
+            'message'   => "予約した",
+        ]);
+    }
 }
--- app/Models/reservation.php
+++ app/Models/reservation.php
@@ -5,7 +5,34 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class reservation extends Model
+class Reservation extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public static function storeReservateion($request)
+    {
+        \DB::transaction(function () use($request){
+            Reservation::create([
+                "screening_date" => $request->screening_date,
+                "schedule_id"    => $request->schedule_id,
+                "sheet_id" => $request->sheet_id,
+                "email"    => $request->email,
+                "name"     => $request->name,
+            ]);
+        });
+    }
+
+    public static function isAllReadyExist($request)
+    {
+        return Reservation::where("schedule_id","=",$request->schedule_id)
+        ->where("sheet_id","=",$request->sheet_id)
+        ->exists();
+    }
 }
+
