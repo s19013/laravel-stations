535f1ec6a7b390eaf1d686117122b6e52db8b7ba
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-12 14:16:08 +0900

    reservationsモデルと紐づけた

--- app/Models/Sheet.php
+++ app/Models/Sheet.php
@@ -9,6 +9,13 @@ class Sheet extends Model
 {
     use HasFactory;
 
+    //belongsTo設定
+    // 所属元を設定
+    public function Reservations()
+    {
+       return $this->belongsTo('App\Models\Reservation');
+    }
+
     public static function getRow($row)
     {
         return Sheet::where('row','=',$row)->get();
