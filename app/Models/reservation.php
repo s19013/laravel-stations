d7f17b35b8bc224dfa8e8b691aaa8cf4b31aaca6
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-12 14:57:32 +0900

    他のモデルと紐づけた すでに予約済みの座席のIDを配列で返す関数作成

--- app/Models/reservation.php
+++ app/Models/reservation.php
@@ -15,6 +15,18 @@ class Reservation extends Model
         'updated_at',
     ];
 
+    //hasMany設定
+    // リレーションシップ設定
+    public function Sheets()
+    {
+        return $this->hasMany('App\Models\Sheet');
+    }
+
+    public function Schedules()
+    {
+        return $this->hasMany('App\Models\Schedule');
+    }
+
     public static function storeReservateion($request)
     {
         \DB::transaction(function () use($request){
@@ -33,6 +45,17 @@ class Reservation extends Model
         return Reservation::where("schedule_id","=",$request->schedule_id)
         ->where("sheet_id","=",$request->sheet_id)
         ->exists();
+    }
+
+    public static function isAllreadyReserved($schedule_id)
+    {
+        $returnValueList = Reservation::select("sheet_id")->where("schedule_id","=",$schedule_id)->get();
+
+        $reservedSheetList =[] ;
+
+        foreach($returnValueList as $returnValue){ array_push($reservedSheetList,$returnValue->sheet_id); }
+
+        return $reservedSheetList;
     }
 }
 
