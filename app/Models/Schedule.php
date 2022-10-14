17f689bd2e3f4dea2ebfd91d7e426a2532787595
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-14 23:55:46 +0900

    screen_idの登録､更新をできるようにした

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -44,6 +44,7 @@ class Schedule extends Model
         DB::transaction(function () use($request){
             Schedule::create([
                 'movie_id'   => $request->movie_id,
+                'screen_id'  => $request->screen_id,
                 'start_time' => $request->start_time_date." ".$request->start_time_time,
                 'end_time'   => $request->end_time_date." ".$request->end_time_time,
             ]);
@@ -55,6 +56,7 @@ class Schedule extends Model
         DB::transaction(function () use($id,$request){
             Schedule::where('id','=',$id)
             ->update([
+                'screen_id'  => $request->screen_id,
                 'start_time' => $request->start_time_date." ".$request->start_time_time,
                 'end_time'   => $request->end_time_date." ".$request->end_time_time,
             ]);
