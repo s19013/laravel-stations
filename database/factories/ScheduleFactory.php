a3c52b5c0190dd73d08fb1ef2553a8ba60cc2295
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-14 23:35:22 +0900

    `schedules`テーブルに`screen_id`を追加

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -16,9 +16,10 @@ class ScheduleFactory extends Factory
     public function definition()
     {
         return [
-            'movie_id' => Movie::factory(),
+            'movie_id'   => Movie::factory(),
+            'screen_id'  => $this->faker->numberBetween(1,3),
             'start_time' => CarbonImmutable::now(),
-            'end_time' => CarbonImmutable::now()->addHours(2),
+            'end_time'   => CarbonImmutable::now()->addHours(2),
         ];
     }
 }
