1fe80c223fcefce66d8c0fdc9c0218dae167d984
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-17 23:17:32 +0900

    user_idを入れる作業追加

--- database/migrations/2022_10_17_230722_add_column_reservations_table.php
+++ database/migrations/2022_10_17_230722_add_column_reservations_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\User;
+use App\Models\Reservation;
+
 return new class extends Migration
 {
     /**
@@ -14,15 +17,20 @@ return new class extends Migration
     public function up()
     {
         Schema::table('reservations', function (Blueprint $table) {
-            // 生成時のみnullableにする(じゃないとエラー吐かれる)
-            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('user_id');
 
             // 外部
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
 
         // usersテーブルを参照して値を入れる
+        $allUser = User::all();
 
+        foreach($allUser as $user){
+            Reservation::where('name','=',$user->name)
+            ->where('email','=',$user->email)
+            ->update(['user_id' => $user->id]);
+        }
     }
 
     /**
