e4865d421f3cb8a5094ac86ffbb6e02e155ed4e7
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-25 23:28:07 +0900

    リポジトリを使うように修正

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Repository\MovieRepository;
+
 use App\Models\Movie;
 
 class MovieController extends Controller
 {
+    public $movieRepository;
+
+    public function __construct(MovieRepository $movieRepository)
+    {
+        $this->movieRepository = $movieRepository;
+    }
+
     public function index(Request $request)
     {
-        $movieList =Movie::search($request);
-        return view('movie.index', ['movieList' => $movieList]);
+        return view('movie.index', ['movieList' => $this->movieRepository->search($request)]);
     }
 }
