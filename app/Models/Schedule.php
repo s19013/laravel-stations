15995ab2a4fb24d5ad9dee2701829aa38413618b
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-26 15:09:49 +0900

    不必要なものけした

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -36,42 +36,4 @@ class Schedule extends Model
     {
        return $this->hasMany('App\Models\Reservation');
     }
-
-
-    // 登録
-    public static function storeSchedule($request)
-    {
-        DB::transaction(function () use($request){
-            Schedule::create([
-                'movie_id'   => $request->movie_id,
-                'screen_id'  => $request->screen_id,
-                'start_time' => $request->start_time_date." ".$request->start_time_time,
-                'end_time'   => $request->end_time_date." ".$request->end_time_time,
-            ]);
-        });
-    }
-
-    public static function updateSchedule($id,$request)
-    {
-        DB::transaction(function () use($id,$request){
-            Schedule::where('id','=',$id)
-            ->update([
-                'screen_id'  => $request->screen_id,
-                'start_time' => $request->start_time_date." ".$request->start_time_time,
-                'end_time'   => $request->end_time_date." ".$request->end_time_time,
-            ]);
-        });
-    }
-
-    public static function deleteSchedule($id)
-    {
-        DB::transaction(function () use($id){
-            Schedule::where('id', '=',$id)->delete();
-        });
-    }
-
-    public static function isExists($id)
-    {
-        return Schedule::where('id','=',$id)->exists();
-    }
 }
